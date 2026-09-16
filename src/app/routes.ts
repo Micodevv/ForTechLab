4a531c9ee669e6920d2e827542089169247abcdb
Micodev47 <micodev47@gmail.com>
2026-09-17 01:11:12 +0200

    corrections pour humaniser le site

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -5,7 +5,7 @@ import FormationDetailPage from "./pages/formation-detail-page";
 import ContactPage from "./pages/contact-page";
 import ServicesPage from "./pages/services-page";
 import FormationsPage from "./pages/formations-page";
-import ProjetsPage from "./pages/projets-page";
+// import ProjetsPage from "./pages/projets-page";
 import EquipePage from "./pages/equipe-page";
 import AboutPage from "./pages/about-page";
 
@@ -19,7 +19,7 @@ export const router = createBrowserRouter([
       { path: "services", Component: ServicesPage },
       { path: "formations", Component: FormationsPage },
       { path: "formation/:id", Component: FormationDetailPage },
-      { path: "projets", Component: ProjetsPage },
+      // { path: "projets", Component: ProjetsPage },
       { path: "equipe", Component: EquipePage },
       { path: "contact", Component: ContactPage },
       { path: "*", Component: HomePage },
